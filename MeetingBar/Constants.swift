6a5ad357c19fc826de00e4e32718eab2d0eedbbb
Jens Goldhammer <j.goldhammer@fme.de>
2020-12-31 14:30:24 +0100

    fix(regex): fix regex for skype selfhosted
    - the new regex will match https://meet.<customer>.<domain> and https://join.<customer>.<domain>, but no other links anymore

--- MeetingBar/Constants.swift
+++ MeetingBar/Constants.swift
@@ -45,7 +45,7 @@ struct LinksRegex {
     let facebook_workspace = try! NSRegularExpression(pattern: #"https://([a-z0-9-.]+)?workplace.com/[^\s]"#)
     let skype = try! NSRegularExpression(pattern: #"https://join.skype.com/[^\s]*"#)
     let skype4biz = try! NSRegularExpression(pattern: #"https://meet.lync.com/[^\s]*"#)
-    let skype4biz_selfhosted = try! NSRegularExpression(pattern: #"https://[meet\.]?[join\.]?[^\s]*"#)
+    let skype4biz_selfhosted = try! NSRegularExpression(pattern: #"https://meet|join\.[^\s]*"#)
     let lifesize = try! NSRegularExpression(pattern: #"https://call.lifesizecloud.com/[^\s]*"#)
 }
 
