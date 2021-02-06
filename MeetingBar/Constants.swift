f1de5d3be2fd254067a9c49e872f4a9728431e8a
leits <leitsius@gmail.com>
2021-02-06 23:23:07 +0100

    Update zoom regex #164

--- MeetingBar/Constants.swift
+++ MeetingBar/Constants.swift
@@ -21,7 +21,8 @@ struct TitleTruncationRules {
 struct LinksRegex {
     let meet = try! NSRegularExpression(pattern: #"https://meet.google.com/[a-z-]+"#)
     let hangouts = try! NSRegularExpression(pattern: #"https://hangouts.google.com/[^\s]*"#)
-    let zoom = try! NSRegularExpression(pattern: #"https:\/\/(?:[a-z0-9-.]+)?zoom.(?:us|com.cn)\/j\/[0-9a-zA-Z?=]*"#)
+    let zoom = try! NSRegularExpression(pattern: #"https:\/\/(?:[a-z0-9-.]+)?zoom.(?:us|com.cn)\/(?:j|my)\/[0-9a-zA-Z?=.]*"#)
+
 
     /**
      * Examples:
