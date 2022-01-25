95622033fbd05daf22f3f25cf990881a507e8835
leits <leitsius@gmail.com>
2022-01-25 12:01:09 +0200

    Use shortenEventTitle by default

--- MeetingBar/Extensions/DefaultsKeys.swift
+++ MeetingBar/Extensions/DefaultsKeys.swift
@@ -41,7 +41,7 @@ extension Defaults.Keys {
 
     // Menu Appearance
     // if the event title in the menu should be shortened or not -> the length will be stored in field menuEventTitleLength
-    static let shortenEventTitle = Key<Bool>("shortenEventTitle", default: false)
+    static let shortenEventTitle = Key<Bool>("shortenEventTitle", default: true)
     static let menuEventTitleLength = Key<Int>("menuEventTitleLength", default: 50)
 
     static let showEventDetails = Key<Bool>("showEventDetails", default: false)
