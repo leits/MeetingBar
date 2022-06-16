650f48c42342e480ec0f9bea40a5db02c8135f2d
quoimec <18281573+quoimec@users.noreply.github.com>
2022-06-16 13:28:32 +1000

    Fixed issue with preference window height

--- MeetingBar/AppDelegate.swift
+++ MeetingBar/AppDelegate.swift
@@ -315,7 +315,7 @@ class AppDelegate: NSObject, NSApplicationDelegate, UNUserNotificationCenterDele
             preferencesWindow.close()
         }
         preferencesWindow = NSWindow(
-            contentRect: NSRect(x: 0, y: 0, width: 700, height: 610),
+            contentRect: NSRect(x: 0, y: 0, width: 700, height: 650),
             styleMask: [.closable, .titled, .resizable],
             backing: .buffered,
             defer: false
