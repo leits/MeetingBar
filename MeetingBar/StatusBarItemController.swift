6ef4bdd4dadf43677a5fbea805a3e9a97f9f52e0
leits <leitsius@gmail.com>
2022-07-22 16:44:13 +0300

    Show checkmark if there is no events for today

--- MeetingBar/StatusBarItemController.swift
+++ MeetingBar/StatusBarItemController.swift
@@ -129,6 +129,10 @@ class StatusBarItemController: NSObject, NSMenuDelegate {
                 guard let nextEvent = nextEvent else {
                     return .none
                 }
+                if Calendar.current.isDateInTomorrow(nextEvent.startDate) {
+                    return .none
+                }
+
                 guard Defaults[.showEventMaxTimeUntilEventEnabled] else {
                     return .nextEvent(nextEvent)
                 }
