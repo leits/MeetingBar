a716bb6afab07b25d1334af167d9827d0d1bfd3f
leits <leitsius@gmail.com>
2023-03-10 14:13:27 +0200

    Fix action buttons in event submenu

--- MeetingBar/StatusBarItemController.swift
+++ MeetingBar/StatusBarItemController.swift
@@ -603,18 +603,18 @@ class StatusBarItemController {
 
             // Send email
             let emailItem = eventMenu.addItem(withTitle: "status_bar_submenu_email_attendees".loco(), action: #selector(emailAttendees), keyEquivalent: "")
-            copyLinkItem.target = self
+            emailItem.target = self
             emailItem.representedObject = event
 
             // Open in App
             let openItem = eventMenu.addItem(withTitle: "status_bar_submenu_open_in_calendar".loco(), action: #selector(openEventInCalendar), keyEquivalent: "")
-            copyLinkItem.target = self
+            openItem.target = self
             openItem.representedObject = event.ID
 
             // Open in fanctastical if fantastical is installed
             if isFantasticalInstalled {
                 let fantasticalItem = eventMenu.addItem(withTitle: "status_bar_submenu_open_in_fantastical".loco(), action: #selector(openEventInFantastical), keyEquivalent: "")
-                copyLinkItem.target = self
+                fantasticalItem.target = self
                 fantasticalItem.representedObject = event
             }
         } else {
