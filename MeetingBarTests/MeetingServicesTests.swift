75cae5fc7fddb598130351e149820e2b90865af1
Jens Gerdes <jens@gerdes.digital>
2023-09-11 16:39:27 +0200

    Added test for Zoom native

--- MeetingBarTests/MeetingServicesTests.swift
+++ MeetingBarTests/MeetingServicesTests.swift
@@ -14,6 +14,7 @@ let meetings = [
     MeetingLink(service: .zoom, url: URL(string: "https://zoom.us/j/5551112222")!),
     MeetingLink(service: .zoom, url: URL(string: "https://any-client.zoom-x.de/j/65194487075")!),
     MeetingLink(service: .zoom_native, url: URL(string: "zoommtg://zoom.us/join?confno=123456789&pwd=xxxx&zc=0&browser=chrome&uname=Betty")!),
+    MeetingLink(service: .zoom_native, url: URL(string: "zoommtg://zoom-x.de/join?confno=123456789&pwd=xxxx&zc=0&browser=chrome&uname=Betty")!),
     MeetingLink(service: .around, url: URL(string: "https://meet.around.co/r/kyafvk1b")!),
     MeetingLink(service: .around, url: URL(string: "https://around.co/r/kyafvk1b")!),
     MeetingLink(service: .blackboard_collab, url: URL(string: "https://us.bbcollab.com/guest/C2419D0F68382D351B97376D6B47ABA2")!),
